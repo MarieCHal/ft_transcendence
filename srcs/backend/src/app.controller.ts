639330c99be67c2b06eda5c1d24b96e96fe9f1c0
jschreye <jschreye@c1r4s2.42lausanne.ch>
2023-04-20 13:15:46 +0200

    finition

---
+++ srcs/backend/src/app.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Post,Body } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Public } from './public';
+import { HttpService } from '@nestjs/axios';
+import { catchError, firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
+import { AuthService } from './auth/services/auth.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService,
+    private readonly httpService: HttpService,
+    private readonly authService: AuthService
+    )  {}
+
+  /*@Public()
+  @Post('wellcome')
+  async helloFriend( @Body() body: any) {
+    console.log(body.code)
+    //console.log("body state:", body.state)
+    const {data} = await firstValueFrom(this.httpService.post(`https://api.intra.42.fr/oauth/token`, {
+            grant_type: 'authorization_code',
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            code: body.code,
+            redirect_uri: 'http://localhost:5173/register',
+            //state: body.state,
+        }).pipe(
+          catchError((error: AxiosError) => {
+            console.error(error.response.data);
+            throw 'An error has happened! ' 
+          })
+        )
+      )
+    console.log("data: ", data);
+
+    return this.authService.registerUser(data.access_token);
+  }*/
+
+}
